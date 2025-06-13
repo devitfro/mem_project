ef41dd1ae6819a139331e85d94143c157590795d
devitfro <alinabogdanovaaaaa@gmail.com>
2025-06-13 15:34:29 +0300

    update

--- src/components/FirstLevelCards.jsx
+++ src/components/FirstLevelCards.jsx
@@ -1,16 +1,16 @@
 import '../styles/LevelCards.css'
 import img01 from '../images/01.jpg'
-import img02 from '../images/02.jpg'
 import img03 from '../images/03.jpg'
 import img04 from '../images/04.jpg'
+import img09 from '../images/09.jpg'
 
 function FirstLevelCards() {
    return (
       <div className="level-container">
          <img src={img01} alt="mem 1" />
-         <img src={img02} alt="mem 2" />
-         <img src={img03} alt="mem 3" />
-         <img src={img04} alt="mem 4" />
+         <img src={img03} alt="mem 2" />
+         <img src={img04} alt="mem 3" />
+         <img src={img09} alt="mem 4" />
       </div>
    );
 }
